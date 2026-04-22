c91e20fd39da73143afcf67be812ff82a146d7d6
Alex Cutforth <alex@densem.net>
2026-04-22 13:26:17 +1200

    fix: task frequencies

--- include/scheduler_task.hpp
+++ include/scheduler_task.hpp
@@ -10,7 +10,7 @@ class SchedulerTask {
     virtual void loop() = 0;
     virtual int get_frequency() const = 0;
 
-    int get_period_micros() const { return 10e6 / this->get_frequency(); };
+    int get_period_micros() const { return 1e6 / this->get_frequency(); };
 
     void log(const char *format, ...);
     void log_err(const char *format, ...);
